d9b5cc37dff12d83071727a5f9bd5ef3e9ec7d99
yohannes.marcell <yohannes.marcell@8c165dae-1281-11df-a229-f598ac1de586>
2010-02-12 02:25:23 +0000

    progress

--- trunk/LZcoding.java
+++ trunk/LZcoding.java
@@ -3,16 +3,6 @@ import java.io.*;
 
 public class LZcoding{
 
-public static void main(String[] args){
-
-
-
-}
-}
-
-
-
-
 public class Trie {
 	int index;
         String chars;
@@ -20,161 +10,65 @@ public class Trie {
         
 
 	public Trie() {
-          data = new String();
+          chars = new String();
           pointers = new ArrayList();
         }            
 
         
 }
 
-public class IO {
-	public static final boolean DEBUGGING = false; // Set to TRUE to print debugging information
-	public static final int BLOCK_SIZE = 128; // Don't touch
+public static void main(String[] args) {
+
+
+/*
+ * 1. read in the file
+ * 2. Put the file in the Trie like data structures..
+ * 3. Compress the file.
+ * 4. Close the file.
+ */
+	char[] inChar;
+	try{
+	if( args[0].equals("c")){
+	compress(args[1]);
+
+	}
+	else if(args[0].equals("d")){ // 
+	decompress(args[1]);
+	}
+	else{
+	throw new Exception();
+	}
+
+
+	}//end try
+
 	
-	// File extensions
-	public static final String COMPRESSED_FILE_EXTENSION = "cpz";
-	public static final String DECOMPRESSED_FILE_EXTENSION = "dcz";
-	
-	/* Pair class definition */
-    static class Pair {
-	    protected boolean valid; 
-	    final protected int index; 
-	    final protected char character;
-		
-	    Pair(int index, char character) {
-			this(index, character, true);
-		}
-		
-		Pair(int index, char character, boolean _v) {
-			assert (((int)character) < BLOCK_SIZE);
-			
-			this.index = index; 
-			this.character = character; 
-			this.setValid(_v);
-		}
-		
-		public int getIndex() {
-			return index;
-		}
-		
-		public char getCharacter() {
-			return character;
-		}
-		
-		public boolean valid() {
-			return valid;
-		}
-		
-		public void setValid(boolean valid) {
-			this.valid = valid;
+	catch(Exception e){
+	System.err.println("exception thrown: " + e.getMessage() );
 		}
 	}
-	
-	/* Decompressor class definition */
-    public static class Decompressor {
-		public boolean finish; 
-		public FileWriter oWriter; 
-		public String lastString; 
-		protected int cursor; 
-		protected Vector output;  
-		
-		Decompressor(String inFile) throws Exception {
-			lastString = "";
-			finish = false; 
-			cursor = 0;
-			output = new Vector(); 
-			FileInputStream inStream =  new FileInputStream(inFile); 
-			FileWriter fw  = new FileWriter(inFile + "." + DECOMPRESSED_FILE_EXTENSION ); 
-			oWriter = fw; 
-			
-			while(true) {
-				int indexHigh = (byte)inStream.read(); 		    
-				if(indexHigh == -1) 
-					break ;
-				int indexMiddle = (byte) inStream.read();		
-				int indexLow = (byte) inStream.read();
-				int index = indexHigh*BLOCK_SIZE*BLOCK_SIZE + indexMiddle*BLOCK_SIZE + indexLow;
-				
-				char c = (char) inStream.read(); 
-				output.add(new Pair(index, c)); 
-			}
-		}
-		
-		public void done() throws Exception {	    
-			oWriter.close(); 
-		}
-		
-		public Pair decode() {
-			Pair ret;
-			if(cursor < output.size()) {
-				ret = (Pair)output.get(cursor++); 
-				ret.setValid(true); 
-			} else {
-				ret = new Pair(0,'a', false); 
-			}
-			return ret; 
-		}  
-		
-		public void append(String op) throws Exception {
-			oWriter.write(lastString); 
-			lastString = new String(op); 
-		}
-	}
-	
-	/* Compressor class definition */
-    static class Compressor {
-		public FileOutputStream oStream; 
-		protected Vector output;  
-		protected File inFile, outFile; 
-	
-		Compressor (String fileName) throws Exception {
-			String outfileName = fileName + "." + COMPRESSED_FILE_EXTENSION; 
-			
-			output  = new Vector(); 
-			outFile = new File(outfileName);
-			oStream = new FileOutputStream(outFile);
-			inFile  = new File(fileName);
-		}
-		
-		public void encode(int index, char character) throws Exception {
-			output.add(new Pair(index, character)); 
-			
-			int xdash     = index / BLOCK_SIZE; 
-			int xLowest   = index % BLOCK_SIZE; 
-			int xdashdash = xdash / BLOCK_SIZE; 
-			int xMiddle   = xdash % BLOCK_SIZE;
-			
-			oStream.write((byte) xdashdash);
-			oStream.write((byte) xMiddle);
-			oStream.write((byte) xLowest);
-			oStream.write(character);
-			
-			if(DEBUGGING) {
-				// This is to help you during debugging, please comment this out for large files
-				System.out.println(" ( " + index + ", " + character + " )");
-			}
-		} 
-		
-		public void done() throws Exception {
-			oStream.close(); 
-		}
-		
-		public char[] getCharacters() throws Exception {
-			char[] ret = new char[(int) (inFile.length()+1)];
-			int retCursor = 0; 
-			FileInputStream from = new FileInputStream(inFile); 
-			
-			while (true) {
-				byte nextByte = (byte) from.read(); 
-				if (nextByte == -1) 
-					break;
-				
-				char nextChar = (char) nextByte; 
-				ret[retCursor++] = nextChar; 
-			}
-			
-			ret[(int)(inFile.length())] = '\000';
-			return ret; 
-		}
-    }
-}
+  
+  public static void compress(String inFile) throws Exception{
+     IO.Compressor compressor = new IO.Compressor(inFile);
+     char[] charArray = compressor.getCharacters();
+
+     //do stuff
+     for(int i=0; i<charArray.length;i++){
+       compressor.encode(i, charArray[i]);
+     }//end for loop
+
+     compressor.done();
+
+  }// end compress
+
+  public static void decompress(String inFile) throws Exception{
+
+  }//end decompress
+}// end class LZCoding
+
+
+
+
+
+
+
