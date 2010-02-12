ed9251937b98f8f093cc84dedbbbdbff1fb67aac
DavidKraftIshihama <DavidKraftIshihama@8c165dae-1281-11df-a229-f598ac1de586>
2010-02-12 21:51:34 +0000

    bug fixed

--- trunk/LZcoding.java
+++ trunk/LZcoding.java
@@ -35,18 +35,30 @@ public static void main(String[] args) {
 	System.err.println("exception thrown: " + e.getMessage() );
 		}
 	}
-  
+  /*
+   The compressor function creates a new IO.Compressor and gets
+   characters from an input file. It creates a Trie to store data.
+   It then loops through all the data, doing the following:
+   	Find the shortest string not stored in the Trie
+        Add this string to the Trie
+        Gets the index to transmit
+        Gets the character to transmit
+        Encode(index, character)
+        Update the loop counter
+   Afterward, it closes the compressor
+   */
   public static void compress(String inFile) throws Exception{
      IO.Compressor compressor = new IO.Compressor(inFile);
      char[] charArray = compressor.getCharacters();
      Trie dictionary = new Trie();
      int num = 0; char ch;
+     
      for(int i=0; i<charArray.length; ){
 	String str = dictionary.find(charArray, i);
 	dictionary.add(str);
 	num = dictionary.getIndex(str);
 	ch = dictionary.getChar(str);
-       compressor.encode(num, ch);
+        compressor.encode(num, ch);
         i += str.length();
 
      }//end for loop
@@ -54,25 +66,40 @@ public static void main(String[] args) {
      compressor.done();
 
   }// end compress
-
+   /*
+   The decompressor function creates a new IO.Decompressor and a new
+   dictionary. It loops over all the data in the inputfile as follows:
+	Get a (index, character) pair from the file
+        Get the index
+	Get the character
+	Get the string from the dictionary with the index
+        Add the character to the string
+        Put the new string in the dictionary
+        Output the string to the output file.
+   At the end, it closes the file.
+   */
   public static void decompress(String inFile) throws Exception{
 	IO.Decompressor io = new IO.Decompressor(inFile);
         HashMap<Integer, String> dictionary = new HashMap<Integer, String>();
         int count = 1;
         dictionary.put(new Integer(0), new String() );
-        String output;
+        String output = "";
         Integer index;
         char character = 1;
-        while(character != 0) {
+        while( character != 0 ) {
  	   IO.Pair next = io.decode();
            index = new Integer(next.getIndex());
            character = next.getCharacter();
+           
            output = dictionary.get(index);
-           output = output + character;
-           dictionary.put(new Integer(count), output);           
+           if (character == 0) io.append(output);
+           else output = output + character;
    	   io.append(output);
+           dictionary.put(new Integer(count), output);
            count++;
   }
+  
+  
   /* Close all relevant files */
   io.done();
 
