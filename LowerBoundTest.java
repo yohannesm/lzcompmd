cfc7855903694522579bdca53c1052c63db4204c
DavidKraftIshihama <DavidKraftIshihama@8c165dae-1281-11df-a229-f598ac1de586>
2010-02-12 20:32:59 +0000

    done

--- LowerBoundTest.java
+++ LowerBoundTest.java
@@ -1,20 +1,33 @@
 import org.junit.* ;
-import static org.junit.Assert.*
+import static org.junit.Assert.*;
 
 public class LowerBoundTest {
 
     @Test
-    public void testLB1() {
-        assertTrue(true);
+    public void testEntropy1() {
+        char[] testArray = "ccccdddd".toCharArray();
+        double entropy = LowerBound.entropy(testArray);
+        assertTrue(entropy == 1.0);
     }
 
     @Test
     public void testLB2() {
-        assertTrue(true);
+        char[] testArray = "".toCharArray();
+        double entropy = LowerBound.entropy(testArray);
+        assertTrue(entropy == 0.0);
     }
 
     @Test
     public void testLB3() {
-        assertTrue(true);
+        char[] testArray = "aaaaaaaa".toCharArray();
+        double entropy = LowerBound.entropy(testArray);
+        assertTrue(entropy == 0.0);
+    }
+
+    @Test
+    public void testLB4() {
+        char[] testArray = "12345678".toCharArray();
+        double entropy = LowerBound.entropy(testArray);
+        assertTrue(entropy == 3.0);
     }
 }
