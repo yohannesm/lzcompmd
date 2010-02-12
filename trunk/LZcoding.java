985a06e15bceaffb0cf3d7af226e6e9bddffbfdb
DavidKraftIshihama <DavidKraftIshihama@8c165dae-1281-11df-a229-f598ac1de586>
2010-02-12 03:43:01 +0000

    GAWEKPROGK

--- trunk/LZcoding.java
+++ trunk/LZcoding.java
@@ -1,89 +1,9 @@
 import java.util.*;
 import java.io.*;
 
+
 public class LZcoding{
 
-public class Trie {
-	int index;
-        String chars;
-        ArrayList<Trie> pointers;
-        ArrayList<Character> nextChar;
-
-	int highestIndex;
-
-	public Trie() {
-	  index = 0;
-	  highestIndex = 0;
-          chars = new String();
-          pointers = new ArrayList<Trie>(128);
-	  nextChar = new ArrayList<Character>(128);
-        }
-
-	public Trie(String input, int in) {
-          index = in;
-	  chars = new String(input);
-	  pointers = new ArrayList<Trie>(128);
-	  nextChar = new ArrayList<Character>(128);
-	}
-
-	public String find(char[] inChar, int ind){
-	//return shortest string not in trie
-        char[] testString;
-	char testChar;
-	boolean end = false;
-	int counter = ind;
-	int pointerIndex;
-	Trie searchTrie = this;
-	while (!end) {
-	    testChar = inChar[counter];
-	    pointerIndex = searchTrie.nextChar.indexOf(new Character(testChar));
-	    if (pointerIndex != -1) {
-	        searchTrie = searchTrie.pointers[pointerIndex];
-		counter++;
-		}
-            else {
-	        end = true;
-            }
-	}
-        return new String(inChar, ind, counter - ind);
-
-	
-	}
-
-	public void add(String input){
-
-	//add the String to the Trie
-	
-	highestIndex++;
-	int ind = 0;
-	Trie target = this;
-	    for(int i = 0; i < input.length() - 1; i++) {
- 	        index = nextChar.indexOf(new Character(input.charAt(i)));
-		target = pointers[index];
-	}
-	target.nextChar.append(input.charAt(input.length() ));
-	target.pointers.append(new Trie(input, highestIndex));
-	}
-
-	public int getIndex(String input){
-	//add the String to the Trie
-        String substr = input.substring(0, input.length() - 1);
-	int ind = 0;
-	Trie target = this;
-	    for(int i = 0; i < substr.length(); i++) {
-	        ind = nextChar.indexOf(new Character(input.charAt(i)));
-		if (ind == -1) { return -1;}
-		target = pointers[index];
-		}
-	return target.index;
-
-	}
-        
-	public char getChar(String input){
-	    return input.charAt[input.length() - 1];
-
-	}
-}
 
 public static void main(String[] args) {
 
@@ -124,11 +44,10 @@ public static void main(String[] args) {
      for(int i=0; i<charArray.length; ){
 	String str = dictionary.find(charArray, i);
 	dictionary.add(str);
-
 	num = dictionary.getIndex(str);
 	ch = dictionary.getChar(str);
-
        compressor.encode(num, ch);
+        i += str.length();
 
      }//end for loop
 
